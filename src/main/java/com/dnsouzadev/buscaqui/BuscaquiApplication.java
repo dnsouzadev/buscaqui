1fc8f108d04128f33c0d38b2494fc65c0a24e326
Daniel Souza <workdndsza@gmail.com>
2024-12-09 10:51:46 -0300

    estrutura inicial

---
+++ src/main/java/com/dnsouzadev/buscaqui/BuscaquiApplication.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.buscaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnsouzadev/buscaqui/BuscaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.buscaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
